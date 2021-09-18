60dcb19ae55241765436de6e0c3ef3ea79eeefa6
Viki Robles <vasiliki.robles@gmail.com>
2021-09-18 13:03:37 +0100

    feat: Fix prettier and eslint issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,53 @@
 module.exports = {
   parser: "@typescript-eslint/parser", // Specifies the ESLint parser
-  parserOptions: {
-    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
-    sourceType: "module", // Allows for the use of imports
-  },
+  plugins: ["prettier", "@typescript-eslint", "react"],
   extends: [
+    "eslint:recommended",
     "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
     "plugin:@typescript-eslint/recommended",
-    "prettier/@typescript-eslint",
     "plugin:prettier/recommended",
   ],
-  rules: {
-    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
-    // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+  settings: {
+    react: {
+      version: "detect",
+    },
   },
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+    jest: true,
+  },
+  globals: {
+    cy: true,
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+    sourceType: "module", // Allows for the use of imports
+  },
+  rules: {
+    "react/prop-types": "off", // Disable prop-types as we use TypeScript for type checking
+    "react/display-name": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
+    ],
+  },
+  overrides: [
+    // Override some TypeScript rules just for .js files
+    {
+      files: ["*.js"],
+      rules: {
+        "@typescript-eslint/no-var-requires": "off", //
+      },
+    },
+  ],
 };
--- prettierrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  semi: false,
-  trailingComma: "all",
-  endOfLine: "lf",
-  singleQuote: true,
-  printWidth: 120,
-  tabWidth: 2,
-};
